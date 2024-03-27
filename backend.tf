e0c2bfffb9e3ab090df0bd91990aa88010a703e4
PajjuPoo <106960473+PajjuPoo@users.noreply.github.com>
2024-03-27 10:04:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
-    storage_account_name = "techtutorialswithpiyush"
+    resource_group_name  = "demo1"
+    storage_account_name = "demostoragesccount2"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
   }
